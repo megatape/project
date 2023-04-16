a96b96d7c29f2aaae8c86d206b2ac740496f63ce
megatape <121339697+megatape@users.noreply.github.com>
2023-04-16 18:42:50 +0300

    Update bot_YAML (1).py

--- bot_YAML (1).py
+++ bot_YAML (1).py
@@ -1,3 +1,4 @@
+import markup as markup
 import telebot
 from telebot import types
 bot = telebot.TeleBot("")
@@ -5,35 +6,54 @@ import time
 import threading
 import schedule
 from notifiers import get_notifier
-import yaml
-from yaml.loader import SafeLoader
-
-data = {}
-
-with open('Test1.txt', 'w', encoding='utf-8') as file:
-    yaml.dump(data, file, indent=4, default_flow_style=False, allow_unicode=True)
-
-with open('Test1.yml',encoding='utf-8') as file:
-    documents = yaml.load(file,Loader=SafeLoader)
-    alola = " ".join(map(str, documents))
-    strings = []
-    print(documents)
-    for key, item in documents.items():
-        strings.append("{}: {}".format(key.capitalize(), item))
-    result = "; ".join(strings)
-    print(result)
+import sqlite3
+# import yaml
+# from yaml.loader import SafeLoader
+#
+# data = {}
+#
+# with open('Test1.txt', 'w', encoding='utf-8') as file:
+#     yaml.dump(data, file, indent=4, default_flow_style=False, allow_unicode=True)
+#
+# with open('Test1.yml',encoding='utf-8') as file:
+#     documents = yaml.load(file,Loader=SafeLoader)
+#     alola = " ".join(map(str, documents))
+#     strings = []
+#     print(documents)
+#     for key, item in documents.items():
+#         strings.append("{}: {}".format(key.capitalize(), item))
+#     result = "; ".join(strings)
+#     print(result)
 
 
 @bot.message_handler(commands=['start'])
 def start(message):
+    connect = sqlite3.connect('bot.db') #название sql файл = bot
+    cursor = connect.cursor()
+# создаём таблицу, в которой будут пользователи
+    cursor.execute("""CREATE TABLE IF NOT EXISTS info(
+        id INTEGER
+    )""")
+
+    connect.commit()
+# проверка на существование уже зарегестрированного пользователя
+    people_id = message.chat.id
+    cursor.execute(f"SELECT id FROM info WHERE id = {people_id}")
+    data = cursor.fetchone()
+    if data is None:
+    # добавление id пользователей
+        bot_id = [message.chat.id]
+        cursor.execute("INSERT INTO info VALUES(?);", bot_id)
+        connect.commit()
+    else:
+        bot.send_message(message.chat.id, 'Вы уже зарегистрированы')
     markup = types.ReplyKeyboardMarkup(resize_keyboard = True)
     item1 = types.KeyboardButton('Расписание')
     item2 = types.KeyboardButton('ДЗ и дедлайны')
-    item3 = types.KeyboardButton('Лекции')
-    item4 = types.KeyboardButton('Раписание экзаменов')
-    item5 = types.KeyboardButton('Полезные ссылки')
+    item3 = types.KeyboardButton('Раписание экзаменов')
+    item4 = types.KeyboardButton('Полезные ссылки')
 
-    markup.add(item1,  item2,  item3,  item4, item5)
+    markup.add(item1,  item2,  item3,  item4)
 
     bot.send_message(message.chat.id, 'Доброго времени суток, {0.first_name}!'.format(message.from_user), reply_markup = markup)
 
@@ -45,13 +65,12 @@ def bot_message(message):
             item1 = types.KeyboardButton('Показать действующее')
             item2 = types.KeyboardButton('Добавить')
             item3 = types.KeyboardButton('Изменить')
-            item4 = types.KeyboardButton('Удалить')
             back = types.KeyboardButton('Назад')
-            markup.add(item1, item2, item3, item4, back)
+            markup.add(item1, item2, item3, back)
             bot.send_message(message.chat.id, 'Расписание', reply_markup=markup)
 
         elif message.text == 'Показать действующее':
-                bot.send_message(message.chat.id,result)
+                bot.send_message(message.chat.id, 'Пока нет\n')
 
 
 
@@ -70,33 +89,13 @@ def bot_message(message):
                             'Записать видос на пп\n')
 
 
-
-
-        elif message.text == 'Лекции':
-            markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
-            item1 = types.KeyboardButton('Действующие лекции')
-            item2 = types.KeyboardButton('Добавить')
-            item3 = types.KeyboardButton('Изменить')
-            item4 = types.KeyboardButton('Удалить')
-            back = types.KeyboardButton('Назад')
-            markup.add(item1, item2, item3, item4, back)
-            bot.send_message(message.chat.id, 'Лекции', reply_markup=markup)
-
-        elif message.text == 'Действующие лекции':
-                bot.send_message(message.chat.id, '...\n')
-
-
-
-
-
         elif message.text == 'Раписание экзаменов':
             markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
             item1 = types.KeyboardButton('Действующие экзамены')
             item2 = types.KeyboardButton('Добавить')
             item3 = types.KeyboardButton('Изменить')
-            item4 = types.KeyboardButton('Удалить')
             back = types.KeyboardButton('Назад')
-            markup.add(item1, item2, item3, item4, back)
+            markup.add(item1, item2, item3, back)
             bot.send_message(message.chat.id, 'Раписание экзаменов', reply_markup=markup)
 
         elif message.text == 'Действующие экзамены':
@@ -110,9 +109,8 @@ def bot_message(message):
             item1 = types.KeyboardButton('Ссылки')
             item2 = types.KeyboardButton('Добавить')
             item3 = types.KeyboardButton('Изменить')
-            item4 = types.KeyboardButton('Удалить')
             back = types.KeyboardButton('Назад')
-            markup.add(item1, item2, item3, item4, back)
+            markup.add(item1, item2, item3, back)
             bot.send_message(message.chat.id, 'Полезные ссылки', reply_markup=markup)
         elif message.text == 'Ссылки':
             bot.send_message(message.chat.id, '1. Физика: https://potatohd.notion.site/c16df808f57f46858156e98ba7e06d29\n'
@@ -123,32 +121,32 @@ def bot_message(message):
             markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
             item1 = types.KeyboardButton('Расписание')
             item2 = types.KeyboardButton('ДЗ и дедлайны')
-            item3 = types.KeyboardButton('Лекции')
-            item4 = types.KeyboardButton('Раписание экзаменов')
-            item5 = types.KeyboardButton('Полезные ссылки')
+            item3 = types.KeyboardButton('Раписание экзаменов')
+            item4 = types.KeyboardButton('Полезные ссылки')
 
-def run_continuously(interval=1):
-    cease_continuous_run = threading.Event()
+            markup.add(item1, item2, item3, item4)
 
-    class ScheduleThread(threading.Thread):
-        @classmethod
-        def run(cls):
-            while not cease_continuous_run.is_set():
-                schedule.run_pending()
-                time.sleep(interval)
+            bot.send_message(message.chat.id, 'Назад', reply_markup=markup)
+bot.polling(none_stop = True)
 
-    continuous_thread = ScheduleThread()
-    continuous_thread.start()
-    return cease_continuous_run
+# def run_continuously(interval=1):
+#     cease_continuous_run = threading.Event()
+#
+#     class ScheduleThread(threading.Thread):
+#         @classmethod
+#         def run(cls):
+#             while not cease_continuous_run.is_set():
+#                 schedule.run_pending()
+#                 time.sleep(interval)
+#
+#     continuous_thread = ScheduleThread()
+#     continuous_thread.start()
+#     return cease_continuous_run
 
-schedule.every().day.at("10:15").do(send_test, 721814824)
-schedule.every().day.at("12:45").do(send_test, 721814824)
-schedule.every().day.at("14:30").do(send_test, 721814824)
-schedule.every().monday.at("8:30").do(send_test, 721814824)
-schedule.every().tuesday.at("16:30").do(send_test, 721814824)
-schedule.every().thursday.at("16:30").do(send_test, 721814824)
-stop_run_continuously = run_continuously()
-
-            markup.add(item1, item2, item3, item4, item5)
-
-            bot.send_message(message.chat.id, 'Назад', reply_markup = markup)
+# schedule.every().day.at("10:15").do(send_test, 721814824)
+# schedule.every().day.at("12:45").do(send_test, 721814824)
+# schedule.every().day.at("14:30").do(send_test, 721814824)
+# schedule.every().monday.at("8:30").do(send_test, 721814824)
+# schedule.every().tuesday.at("16:30").do(send_test, 721814824)
+# schedule.every().thursday.at("16:30").do(send_test, 721814824)
+# stop_run_continuously = run_continuously()
